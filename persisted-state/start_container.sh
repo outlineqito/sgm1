d681d372ef8d0f8887e40063f1822e948fb4fb89
outlineqito <outlineqito@gmail.com>
2025-08-22 18:43:36 +0000

    first commit

--- persisted-state/start_container.sh
+++ persisted-state/start_container.sh
@@ -28,8 +28,8 @@ docker_command=(
   -e "SB_STATE_DIR=/opt/outline/persisted-state"
 
   # Port number and path prefix used by the server manager API.
-  -e "SB_API_PORT=21834"
-  -e "SB_API_PREFIX=6a1Iw9SLH0w78o9PXRN43g"
+  -e "SB_API_PORT=18988"
+  -e "SB_API_PREFIX=XtJF1k-KmtWomHFZT2xtVw"
 
   # Location of the API TLS certificate and key.
   -e "SB_CERTIFICATE_FILE=/opt/outline/persisted-state/shadowbox-selfsigned.crt"
